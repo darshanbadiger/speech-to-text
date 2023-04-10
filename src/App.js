8f81fd9681e85344c58b3503833f985309515f95
Darshan Badiger <darshanabadiger@gmail.com>
2023-04-10 10:43:21 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
+import useClipboard from "react-use-clipboard";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [textcopy, setTextcopy] = useState();
+  const [isCopied, setCopied] = useClipboard(textcopy);
+
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
+
+  if (!browserSupportsSpeechRecognition) {
+    return <span>Browser doesn't support speech recognition.</span>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h2>Speech to Text Converter</h2>
+        <p>React Hook that converts speech to text and copies to clipboard.</p>
+
+        <div className="main-content" onClick={() => setTextcopy(transcript)}>
+          {transcript}
+        </div>
+
+        <div className="btn-style">
+          <button onClick={setCopied}>
+            {isCopied ? "Copied!" : "Copy to Clipboard"}
+          </button>
+          <button onClick={startListening}>Start Listining</button>
+          <button onClick={SpeechRecognition.stopListening}>
+            Stop Listining
+          </button>
+        </div>
+      </div>
+    </>
   );
 }
-
-export default App;
